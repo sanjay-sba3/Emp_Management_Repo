b997c21bcf6e6dbdfbc7059b379bf0c7e2e07bf9
SanjayAkhade <sb.akhade3@gmail.com>
2024-08-29 22:36:16 +0530

    userapi is added for user page

--- employee_management/user/views/views.py
+++ employee_management/user/views/views.py
@@ -19,15 +19,17 @@ class LoginAPI(generics.GenericAPIView):
         try:
             username = request.data.get("username")
             password = request.data.get("password")
-            print(username, password)
+            # print(username, password)
              # Authenticate user using Django's built-in method
             user = authenticate(request, username=username, password=password)
+
             data = {}
             if user:
                 # user_serializer = UserSerailizer(user).data
                 # jwt tokenGenerate refresh token
-                
+                login_user = User.objects.get(username=username)
                 access_token = AccessToken.for_user(user)
+                data["username"] = user.username
                 data["access_token"]=str(access_token)
                 return Response(
                     {"message": "Logged in successfully", "data": data, "status": 200},
@@ -35,14 +37,39 @@ class LoginAPI(generics.GenericAPIView):
                 )
             else:
                 return Response(
-                    {"message": "Username or password is incorrect", "data": [], "status": 401},
-                    status=status.HTTP_401_UNAUTHORIZED
+                    {"message": "Username or password is incorrect", "data": [], "status": status.HTTP_401_UNAUTHORIZED},
                 )
         except Exception as e:
             print(str(e))
             # raise ("login failed",400 )
 
             return Response(
-                {"message": "Login failed", "data": [], "status": 500},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                {"message": "Login failed", "data": [], "status": status.HTTP_500_INTERNAL_SERVER_ERROR},
+            )
+        
+
+
+
+class UserAPIView(generics.GenericAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+    def get(self, request):
+        
+        try:
+            username = request.query_params.get("username", None) 
+            data = {}
+            if username:
+                user = User.objects.get(username=username)
+                username_serializer = UserSerailizer(user).data
+                data["username"] = username_serializer["username"]
+                data["email"] = username_serializer["email"]
+                return Response({'message':"Success", 'data':data, 'status': status.HTTP_200_OK})
+            else:
+                return Response({"error":"User not found", "status" : status.HTTP_401_UNAUTHORIZED})
+        except Exception as e:
+            return Response(
+                {"message": "Something went wrong", "data": [], "status":status.HTTP_500_INTERNAL_SERVER_ERROR},
+                
             )
